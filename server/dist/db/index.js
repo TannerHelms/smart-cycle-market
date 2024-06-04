427dfbc83ea0776767af702ee5cf4b7d3c8ba828
TannerHelms <tannerhelms01@gmail.com>
2024-06-04 15:25:05 -0600

    removed dist, update .env.example

--- server/dist/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const uri = 'mongodb://localhost:27017/smart-cycle-market';
-(0, mongoose_1.connect)(uri).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log('Failed to connect to MongoDB: ', err.message);
-});
