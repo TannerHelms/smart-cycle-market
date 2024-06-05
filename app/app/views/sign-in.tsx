03400fcbf0a8c7fe4f336376f1fb14e0c5407e2a
TannerHelms <tannerhelms01@gmail.com>
2024-06-05 00:34:52 -0600

    added views

---
+++ app/app/views/sign-in.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export interface SignInProps {
+}
+
+export function SignIn(props: SignInProps) {
+    return (
+        <View>
+            <Text>SignIn</Text>
+        </View>
+    );
+}
